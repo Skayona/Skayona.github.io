c50c805f8561fc63be424f3c01b7986fa1383ceb
Kateryna Metelova <skaya.online@gmail.com>
2017-06-15 04:13:23 +0300

    redone after commnets

--- StenserBrothers_Metelova/js/script.js
+++ StenserBrothers_Metelova/js/script.js
@@ -8,3 +8,24 @@ $('.js-hamburger').click(function() {
 		$('.menu-wrap').slideDown(400)
 	}
 })
+
+$('.js-scrollto').click(function () {
+	var elementClick = $(this).attr('href')
+	var destination = $(elementClick).offset().top;
+	$('html:not(:animated),body:not(:animated)').animate({
+		scrollTop: destination}, 800);
+	return false;
+});
+
+$('.js-top').click(function() {  
+	$('body,html').animate({scrollTop:0},500);  
+	return false;
+});
+
+$(window).scroll(function() {
+	if($(this).scrollTop() > 150) {
+		$('.js-top').show(800);
+	} else {
+		$('.js-top').hide();
+	}
+});

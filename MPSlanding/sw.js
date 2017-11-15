437127ff81c544722f559cb884c09ade2c01d28d
Unknown <skaya.online@gmail.com>
2017-11-15 17:05:55 +0200

    added optimization

---
+++ MPSlanding/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(event) {
+
+	event.waitUntil(
+		caches.open('static').then(function(cache) {
+			return cache.addAll([
+				'/img/logo.svg',
+				'/img/favicon.png',
+				'/js/script.min.js',
+				'/css/style.min.css',
+				'/fonts/iconfont/iconfont.svg'
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			if (response) {
+				return response;
+			};
+			return fetch(event.request);
+		})
+	);
+});

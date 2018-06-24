06e9611d0fafeca8bf7f719dcb900f4ee4a1dd94
Unknown <skaya.online@gmail.com>
2018-06-24 22:32:56 +0300

    wip

--- ols/php/send.php
+++ ols/php/send.php
@@ -9,8 +9,8 @@ function isEmail($email)
 if ($_POST) {
 
    // Enter the email where you want to receive the message
-	$emailTo = 'catmallua@gmail.com'; //test
-	$emailTo = '';
+	$emailTo = 'skaya.online@gmail.com'; //test
+	// $emailTo = '';
 
 
 	$clientEmail = addslashes(trim($_POST['email']));
@@ -19,15 +19,14 @@ if ($_POST) {
 	$clientSubject = addslashes(trim($_POST['subject']));
 	$clientMessage = addslashes(trim($_POST['message']));
 	$clientPage = addslashes(trim($_POST['form']));
-	$antispam = addslashes(trim($_POST['check']));
-	if ($antispam != 'isChecked') exit ('Spam decected');
+
 
 
 
 	if (isEmail($clientEmail)) {
-		$headers = "Form: " . $page . "\r\n" . "From: ". "$clientEmail" . "\r\n" . "Content-type:text/plain;charset=utf-8" . "\r\n";
-		$subject = "New message from '". $page ."'";
-		$emailMessage = "Email: " . $clientEmail . "\r\n". "Name: " . $clientName  . "\r\n" . "Tel: " . $clientTel . "\r\n" . "Subject: " . $clientSubject . "\r\n". "Message: " . $clientMessage . "\r\n" . "From form: " . $page;
+		$headers = "Form: " . $clientPage . "\r\n" . "From: ". "$clientEmail" . "\r\n" . "Content-type:text/plain;charset=utf-8" . "\r\n";
+		$subject = "New message from '". $clientPage ."'";
+		$emailMessage = "Email: " . $clientEmail . "\r\n". "Name: " . $clientName  . "\r\n" . "Tel: " . $clientTel . "\r\n" . "Subject: " . $clientSubject . "\r\n". "Message: " . $clientMessage . "\r\n" . "From form: " . $clientPage;
 		mail($emailTo, $subject, $emailMessage, $headers);
 	}
 

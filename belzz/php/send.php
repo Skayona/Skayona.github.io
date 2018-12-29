bebe36f516379affc0a1a84cfabe23f2bff7e084
Catrina <skaya.online@gmail.com>
2018-12-29 14:25:23 +0200

    wip

--- belzz/php/send.php
+++ belzz/php/send.php
@@ -1,8 +1,8 @@
 <?php
 if ($_POST) {
    // Enter the email where you want to receive the message
-	$emailTo1 = 'skaya.online@gmail.com'; //test
-	$emailTo2 = 'catmallua@gmail.com'; //test
+	$emailTo1 = 'anevesenko17@yandex.ru'; 
+	// $emailTo2 = 'catmallua@gmail.com'; //test
 	$name = addslashes(trim($_POST['callback-name']));
 	$tel = addslashes(trim($_POST['callback-tel']));
 
@@ -11,7 +11,7 @@ if ($_POST) {
   $subject = "Новый запрос на обратный звонок: ". $name .", ".$tel;
   $emailMessage = "Name: " . $name . "\r\n". "Tel: " . $tel;
   mail($emailTo1, $subject, $emailMessage, $headers);
-  mail($emailTo2, $subject, $emailMessage, $headers);
+  // mail($emailTo2, $subject, $emailMessage, $headers);
 
 	echo json_encode($array);
 }

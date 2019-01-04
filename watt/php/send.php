dcae2a1de40dda0fcab752b7d2621460fc70dcea
Catrina <skaya.online@gmail.com>
2019-01-04 02:04:45 +0200

    wip

--- watt/php/send.php
+++ watt/php/send.php
@@ -2,16 +2,17 @@
 if ($_POST) {
    // Enter the email where you want to receive the message
 	$emailTo1 = 'skaya.online@gmail.com'; //test
-	$emailTo2 = 'catmallua@gmail.com'; //test
+	// $emailTo2 = 'catmallua@gmail.com'; //test
 	$name = addslashes(trim($_POST['callback-name']));
-	$tel = addslashes(trim($_POST['callback-tel']));
+  $tel = addslashes(trim($_POST['callback-tel']));
+  $comment = addslashes(trim($_POST['callback-comment']));
 
 
   $headers = "New request for callback" . "\r\n" . "Content-type:text/plain;charset=utf-8" . "\r\n";
   $subject = "Новый запрос на обратный звонок: ". $name .", ".$tel;
-  $emailMessage = "Name: " . $name . "\r\n". "Tel: " . $tel;
+  $emailMessage = "Name: " . $name . "\r\n". "Tel: " . $tel . "\r\n". "Comment: " . $comment;
   mail($emailTo1, $subject, $emailMessage, $headers);
-  mail($emailTo2, $subject, $emailMessage, $headers);
+  // mail($emailTo2, $subject, $emailMessage, $headers);
 
 	echo json_encode($array);
 }

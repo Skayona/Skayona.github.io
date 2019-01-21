065ddb6a4f21f570b8f0ac74721ee9f9e919e71f
Catrina <skaya.online@gmail.com>
2019-01-21 12:51:49 +0200

    add fixes wq

--- watt/php/send.php
+++ watt/php/send.php
@@ -1,21 +1,20 @@
 <?php
 if ($_POST) {
-   // Enter the email where you want to receive the message
-	$emailTo1 = 'info@wattdev.ru';
-  // $emailTo2 = '';
+  // Enter the email where you want to receive the message
+  $emailTo = 'sales@wattdev.ru';
 
-	$name = addslashes(trim($_POST['callback-name']));
+  $name = addslashes(trim($_POST['callback-name']));
   $tel = addslashes(trim($_POST['callback-tel']));
   $comment = addslashes(trim($_POST['callback-comment']));
 
 
-  $headers = "New request for callback" . "\r\n" . "Content-type:text/plain;charset=utf-8" . "\r\n";
-  $subject = "Новый запрос на обратный звонок: ". $name .", ".$tel;
+  $headers = "Content-type:text/plain;charset=utf-8" . "\r\n" .
+	"From: feedback@wattdev.ru" . "\r\n";
+  $subject = "Customer request: ". $name .", ".$tel;
   $emailMessage = "Name: " . $name . "\r\n". "Tel: " . $tel . "\r\n". "Comment: " . $comment;
-  mail($emailTo1, $subject, $emailMessage, $headers);
-  // mail($emailTo2, $subject, $emailMessage, $headers);
+  mail($emailTo, $subject, $emailMessage, $headers);
 
-	echo json_encode($array);
+  echo json_encode($array);
 }
 ?>
 
